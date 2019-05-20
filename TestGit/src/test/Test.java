71b90063c5a21769b3ba4f6af3b69799bafff79b
jainne <jainne@INGUEUCLP000666.camkmg-bangalore.com>
2019-05-20 16:56:47 +0530

    Code changes

--- TestGit/src/test/Test.java
+++ TestGit/src/test/Test.java
@@ -2,9 +2,10 @@ package test;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
+System.out.println("Neeraj");
 	}
 
 }
